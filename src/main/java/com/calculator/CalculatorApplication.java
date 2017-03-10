7094a22d807f60df288acaa1290a5b49a69300a0
Omar Mujahid <omar_mjhd@yahoo.com>
2017-03-09 19:06:41 -0600

    CORS filter tests

--- src/main/java/com/calculator/CalculatorApplication.java
+++ src/main/java/com/calculator/CalculatorApplication.java
@@ -7,6 +7,10 @@ import com.google.inject.Injector;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 import com.calculator.resources.CalculatorResource;
 
@@ -29,6 +33,17 @@ public class CalculatorApplication extends Application<CalculatorConfiguration> 
     @Override
     public void run(CalculatorConfiguration configuration, Environment environment) throws Exception {
 
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
         Injector injector = Guice.createInjector(new CalculatorModule(configuration));
 
